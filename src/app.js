1f9dc7a97ab416ea4f8a2d02b58b2fd353f47ca1
Yu Mao <maoyu960320@hotmail.com>
2019-01-14 15:55:59 +0800

    add controls

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 import '@babel/polyfill';
 import linearRegression from './linear-regression';
+import './style.css';
 
-linearRegression();
+const trueCoefficient = { a: 1, b: 1, c: 1 };
+linearRegression(trueCoefficient);
+
+const items = Array.from(document.querySelectorAll('.item'));
+
+items.map(item => {
+  item.addEventListener('change', e => {
+    const type = item.dataset.type;
+    trueCoefficient[type] = parseInt(e.target.value, 10);
+    linearRegression(trueCoefficient);
+  });
+});
